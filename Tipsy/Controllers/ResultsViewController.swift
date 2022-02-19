f30880ca4e9dc67baaf402450e1c8d74416f2bdd
brGuirra <br.guirra@gmail.com>
2022-02-18 22:06:43 -0300

    feat: add IBOutlets to ResultsViewController

---
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Bruno Guirra on 18/02/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    @IBOutlet var totalLabel: UILabel!
+    @IBOutlet var settingsLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+}
